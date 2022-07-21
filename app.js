ad488d7a3b3bd7530b49e00b5b086bf696949f75
rpatterson459 <rpatterson459@gmail.com>
2022-07-20 17:52:27 -0700

    completed all

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
+console.log("connected!")
+const quoteText = document.querySelector(".quote"),
+authorName = document.querySelector(".name"),
+quoteBtn = document.querySelector("button");
+soundBtn = document.querySelector(".sound"),
+copyBtn = document.querySelector(".copy"),
 
+
+
+
+soundBtn.addEventListener("click", ()=>{
+    let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innertext}`);
+    speechSynthesis.speak(utterance);
+});
+
+copyBtn.addEventListener("click", ()=>{
+    navigator.clipboard.writeText(quoteText.innerHTML)
+});
+
+quoteBtn.addEventListener("click", function randomQuote(){
+ //   quoteBtn.classicList.add("loading");//
+   // quoteBtn.innerText = "Loading Quote..."
+    fetch("https://api.quotable.io/random").then(result => result.json()).then(result =>{
+        console.log(result)
+        quoteText.innerHTML = result.content;
+        authorName.innerHTML = result.author;
+        quoteBtn.innerHTML = "New Quote";
+      //  quoteBtn.classicList.remove("loading");
+    });
+   // console.log(result)
+});
